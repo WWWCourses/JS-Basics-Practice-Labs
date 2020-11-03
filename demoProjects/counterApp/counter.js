79e95dfbc994973219e6baffc073851e5693401c
WWWCourses <ivawebcourses@gmail.com>
2020-11-03 13:52:23 +0200

    Lab7: 03.11.2020 DONE: https://wwwcourses.github.io/JS-Basics-Practice-Slides/pages/themes/JS_Events/JS_Events.html#/4

--- demoProjects/counterApp/counter.js
+++ demoProjects/counterApp/counter.js
@@ -1,6 +1,17 @@
-// let number = document.querySelector('.number');
-// // console.log( number.innerHTML );
-// number.innerHTML = 1;
+let initialCount = 1;
 
+// set initial counter value:
+let counter = document.getElementById("counter");
+counter.innerHTML = initialCount;
 
-// setTimeout(() => {console.log(`Hello`)}, 1000);
+// on "+" button click, increment counter:
+{/* <button class="increment">+</button> */}
+let incBtn = document.querySelector(".increment");
+incBtn.addEventListener('click', incrementCounter );
+
+function incrementCounter() {
+	initialCount += 1;
+	counter.innerHTML = initialCount;
+}
+
+// TASK: implement decrementCounter on "-" click
---
+++ demoProjects/counterApp/tmp.js
@@ -0,0 +1,17 @@
+let arr = [
+	{
+		id: 1
+	},
+	{
+		id: 2
+	}
+]
+
+// console.log( arr[0].id );
+// console.log( arr[1].id );
+
+
+for(let i=0; i<arr.length;i++){
+	console.log( arr[i].id );
+}
+
